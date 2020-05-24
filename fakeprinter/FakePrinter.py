89026a2e8d3db20976d1567a53b69fef6e1e30b3
Luc Rubio <luc@loociano.com>
2020-05-24 10:18:38 +0100

    FakePrinter: log responses.

--- fakeprinter/FakePrinter.py
+++ fakeprinter/FakePrinter.py
@@ -3,21 +3,30 @@ Copyright 2020 Luc Rubio <luc@loociano.com>
 Plugin is licensed under the GNU Lesser General Public License v3.0.
 """
 import logging
+import socketserver
 from http.server import HTTPServer, BaseHTTPRequestHandler
+
+from typing import Tuple
 
 
 class FakePrinter:
   """Emulates the Monoprice Select Mini V2 HTTP REST API."""
 
   def __init__(self):
-    self.logger = logging.getLogger('FakePrinter')
+    self._logger = logging.getLogger('FakePrinter')
     logging.basicConfig(level=logging.INFO)
 
   class Handler(BaseHTTPRequestHandler):
     """Handles HTTP requests."""
 
+    def __init__(self, request: bytes, client_address: Tuple[str, int],
+                 server: socketserver.BaseServer) -> None:
+      """Constructor."""
+      self._logger = logging.getLogger('FakePrinterHandler')
+      super().__init__(request, client_address, server)
+
     # pylint:disable=invalid-name
-    def do_GET(self):
+    def do_GET(self) -> None:
       """Handles HTTP GET requests."""
       self.send_response(200)
       self.send_header('Content-type', 'text/html')
@@ -26,12 +35,13 @@ class FakePrinter:
         response = 'T0/0P0/0/0I'
       else:
         response = 'OK'
+      self._logger.info('Response: %s', response)
       self.wfile.write(response.encode('utf-8'))
 
-  def run(self, server_class=HTTPServer, handler_class=Handler):
+  def run(self, server_class=HTTPServer, handler_class=Handler) -> None:
     """Runs the fake printer."""
     server_address = ('', 80)
-    self.logger.info('Starting fake printer on http://127.0.0.1:80')
+    self._logger.info('Starting fake printer on http://localhost:80')
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
 
