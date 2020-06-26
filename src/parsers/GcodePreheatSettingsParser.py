780be5aa6b34c82463b1a5e59d0c2f4cc20fb6f9
Luc Rubio <luc@loociano.com>
2020-06-26 21:58:22 +0100

    Add parser to detect preheat settings from g-code instructions.

---
+++ src/parsers/GcodePreheatSettingsParser.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2020 Luc Rubio <luc@loociano.com>
+Plugin is licensed under the GNU Lesser General Public License v3.0.
+"""
+import re
+from typing import Tuple
+
+
+class GcodePreheatSettingsParser:
+  """Parses preheat settings from a list of g-code commands."""
+
+  @staticmethod
+  def parse(gcode: bytes) -> Tuple[int, int]:
+    """Parses preheating temperatures from gcode.
+
+    Args:
+      gcode: UTF-8 byte stream
+    Returns
+      Tuple with preheat bed and hotend temperature. Both can be None.
+    """
+    bed_temperature, hotend_temperature = None, None
+
+    for line in gcode.decode('utf-8').splitlines():
+      if bed_temperature is not None and hotend_temperature is not None:
+        return bed_temperature, hotend_temperature
+
+      if bed_temperature is None:
+        # M190 = Wait for bed temperature to reach target temperature.
+        bed_match = re.match(r"^.*M190 S(\d+).*$", line)
+        if bed_match is not None:
+          bed_temperature = int(bed_match.group(1))
+
+      if hotend_temperature is None:
+        # M109 = Set Extruder Temperature and wait.
+        hotend_match = re.match(r"^.*M109 S(\d+).*$", line)
+        if hotend_match is not None:
+          hotend_temperature = int(hotend_match.group(1))
+
+    return bed_temperature, hotend_temperature
---
+++ test/parsers/test_GcodePreheatSettingsParser.py
@@ -0,0 +1,46 @@
+"""
+Copyright 2020 Luc Rubio <luc@loociano.com>
+Plugin is licensed under the GNU Lesser General Public License v3.0.
+"""
+import unittest
+
+from src.parsers.GcodePreheatSettingsParser import GcodePreheatSettingsParser
+
+
+class GcodePreheatSettingsParserTest(unittest.TestCase):
+  def test_parseBedAndHotendTemperature_success(self):
+    gcode = """
+    ;FLAVOR:Marlin
+    M140 S65
+    M105
+    M190 S65
+    M104 S205
+    M105
+    M109 S205 ;a comment
+    M82"""
+    self.assertTupleEqual(
+        (65, 205), GcodePreheatSettingsParser.parse(str.encode(gcode)))
+
+  def test_parseOnlyPreheatBedTemperature_success(self):
+    gcode = """
+    M190 S65
+    M190 S0
+    """
+    self.assertTupleEqual(
+        (65, None), GcodePreheatSettingsParser.parse(str.encode(gcode)))
+
+  def test_parseOnlyPreheatHotendTemperature_success(self):
+    gcode = """
+    M109 S200
+    M109 S0
+    """
+    self.assertTupleEqual(
+        (None, 200), GcodePreheatSettingsParser.parse(str.encode(gcode)))
+
+  def test_parseWithoutPreheatCommands_empty(self):
+    self.assertTupleEqual(
+        (None, None), GcodePreheatSettingsParser.parse(str.encode('M82')))
+
+  def test_parseNoInput_empty(self):
+    self.assertTupleEqual(
+        (None, None), GcodePreheatSettingsParser.parse(str.encode('')))
