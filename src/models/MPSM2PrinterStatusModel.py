16753e4a08ebbc62c1195ad4f9708023c5bc27d9
Luc Rubio <luc@loociano.com>
2021-09-26 21:27:46 +0100

    Add default status model values and validate
    Write tests.

--- src/models/MPSM2PrinterStatusModel.py
+++ src/models/MPSM2PrinterStatusModel.py
@@ -8,25 +8,48 @@ import enum
 class MPSM2PrinterStatusModel:
   """Printer's Status Model."""
 
+  MAX_HOTEND_TEMPERATURE = 260  # Degrees Celsius.
+  MAX_BED_TEMPERATURE = 85  # Degrees Celsius.
+
   class State(enum.Enum):
     """Printer State."""
-    UNKNOWN = 1
-    IDLE = 2
-    PRINTING = 3
+    UNKNOWN = enum.auto()
+    IDLE = enum.auto()
+    PRINTING = enum.auto()
 
-  def __init__(self, hotend_temperature: int, target_hotend_temperature: int,
-               bed_temperature: int, target_bed_temperature: int, progress: int,
-               state: State) -> None:
+  def __init__(self,
+               hotend_temperature: int = 0,
+               target_hotend_temperature: int = 0,
+               bed_temperature: int = 0,
+               target_bed_temperature: int = 0,
+               progress: int = 0,
+               state: State = State.IDLE) -> None:
     """Constructor.
 
     Args:
-      hotend_temperature: From 0 to 280.
-      target_hotend_temperature: From 0 to 280.
-      bed_temperature: From 0 to 85.
-      target_bed_temperature: From 0 to 85.
-      progress: From 0 to 100
-      state: Idle or printing.
+      hotend_temperature: From 0 to 260 degrees Celsius.
+      target_hotend_temperature: From 0 to 260 degrees Celsius.
+      bed_temperature: From 0 to 85 degrees Celsius.
+      target_bed_temperature: From 0 to 85 degrees Celcsius.
+      progress: print progress percentage, from 0 to 100.
+      state: state the printer is in (e.g. idle, printing).
     """
+    if (hotend_temperature < 0
+        or hotend_temperature > self.MAX_HOTEND_TEMPERATURE):
+      raise ValueError(f'Invalid hotend temperature: {hotend_temperature}.')
+    if (target_hotend_temperature < 0
+        or target_hotend_temperature > self.MAX_HOTEND_TEMPERATURE):
+      raise ValueError(
+          f'Invalid target hotend temperature: {target_hotend_temperature}.')
+    if (bed_temperature < 0
+        or bed_temperature > self.MAX_BED_TEMPERATURE):
+      raise ValueError(f'Invalid bed temperature: {bed_temperature}.')
+    if (target_bed_temperature < 0
+        or target_bed_temperature > self.MAX_BED_TEMPERATURE):
+      raise ValueError(
+          f'Invalid target bed temperature: {target_bed_temperature}')
+    if progress < 0 or progress > 100:
+      raise ValueError(f'Invalid printing progress: {progress}.')
     self.hotend_temperature = hotend_temperature
     self.target_hotend_temperature = target_hotend_temperature
     self.bed_temperature = bed_temperature
---
+++ test/models/TestMPSM2PrinterStatusModel.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2021 Luc Rubio <luc@loociano.com>
+Plugin is licensed under the GNU Lesser General Public License v3.0.
+"""
+import unittest
+
+from src.models.MPSM2PrinterStatusModel import MPSM2PrinterStatusModel
+
+
+class MPSM2PrinterStatusModelTest(unittest.TestCase):
+  def test_invalidHotendTemperature_fails(self):
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(hotend_temperature=-1)
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(hotend_temperature=261)
+
+  def test_invalidTargetHotendTemperature_fails(self):
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(target_hotend_temperature=-1)
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(target_hotend_temperature=261)
+
+  def test_invalidBedTemperature_fails(self):
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(bed_temperature=-1)
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(bed_temperature=86)
+
+  def test_invalidTargetBedTemperature_fails(self):
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(target_bed_temperature=-1)
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(target_bed_temperature=86)
+
+  def test_invalidProgress_fails(self):
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(progress=-1)
+    with self.assertRaises(ValueError):
+      MPSM2PrinterStatusModel(progress=101)
